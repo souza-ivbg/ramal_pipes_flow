9dae8beadea9412dee47972be84f3347304e9d42
Isaac V. Souza <104950964+isaac-newtonian@users.noreply.github.com>
2025-07-11 14:23:08 -0300

    first save

---
+++ test.py
@@ -0,0 +1,10 @@
+'''
+Essa é uma seção de teste
+
+a principio o codigo ira funcionar da seguinte forma
+
+inicia-se oas tubulações
+o usuario identifica como resolver o problema
+o programa serve como uma calculadora
+
+'''
